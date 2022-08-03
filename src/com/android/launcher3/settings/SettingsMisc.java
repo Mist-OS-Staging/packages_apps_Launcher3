7efc86d4dd11467dc0e652870e322f29a54d1829
Pranav Vashi <neobuddy89@gmail.com>
2022-08-04 00:15:07 +0530

    Launcher3: Remove blur radius option if not supported
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/settings/SettingsMisc.java
+++ src/com/android/launcher3/settings/SettingsMisc.java
@@ -52,6 +52,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.launcher3.BuildConfig;
 import com.android.launcher3.LauncherFiles;
+import com.android.launcher3.LauncherPrefs;
+import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.lineage.trust.TrustAppsActivity;
@@ -60,6 +63,8 @@ import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.SettingsCache;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+
+import com.android.systemui.shared.system.BlurUtils;
 
 /**
  * Settings activity for Launcher.
@@ -238,7 +243,18 @@ public class SettingsMisc extends CollapsingToolbarBaseActivity
          * will remove that preference from the list.
          */
         protected boolean initPreference(Preference preference) {
+            String key = preference.getKey();
+            if (key == null) {
+                return true;
+            }
+
+            if (key.equals(LauncherPrefs.BLUR_DEPTH.getSharedPrefKey())) {
+                return BlurUtils.supportsBlursOnWindows();
+            }
+
+            DisplayController.Info info = DisplayController.INSTANCE.get(getContext()).getInfo();
             switch (preference.getKey()) {
+
                 case ALLOW_ROTATION_PREFERENCE_KEY:
                     DisplayController.Info info =
                             DisplayController.INSTANCE.get(getContext()).getInfo();
