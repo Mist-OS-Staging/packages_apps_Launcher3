Musical143143/android_packages_apps_Launcher3

[ICONPROVIDER] add lawnchair icon support
Change-Id: Ide7d58ab39246955aee20471557c8aaa0f7984cf
Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

src/com/android/launcher3/icons/LauncherIconProvider.java
@@ -15,8 +15,14 @@
  */
 package com.android.launcher3.icons;
 
+import static com.android.launcher3.util.Executors.MAIN_EXECUTOR;
+
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.graphics.drawable.AdaptiveIconDrawable;
@@ -29,6 +35,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.android.internal.util.android.Utils;
 import com.android.launcher3.R;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.dagger.ApplicationContext;
@@ -36,6 +43,7 @@ import com.android.launcher3.dagger.LauncherAppSingleton;
 import com.android.launcher3.graphics.ShapeDelegate;
 import com.android.launcher3.graphics.ThemeManager;
 import com.android.launcher3.util.ApiWrapper;
+import com.android.launcher3.util.SafeCloseable;
 
 import org.xmlpull.v1.XmlPullParser;
 
@@ -57,10 +65,14 @@ public class LauncherIconProvider extends IconProvider {
     private static final String TAG = "LIconProvider";
     private static final Map<String, ThemeData> DISABLED_MAP = Collections.emptyMap();
 
+    private static final String LAWNICONS_PACKAGE = "app.lawnchair.lawnicons";
+    private static final String LAWNICONS_ICON_MAP_XML = "grayscale_icon_map";
+
     private Map<String, ThemeData> mThemedIconMap;
 
     private final ApiWrapper mApiWrapper;
     private final ThemeManager mThemeManager;
+    private final Context mContext;
 
     @Inject
     public LauncherIconProvider(
@@ -68,9 +80,11 @@ public class LauncherIconProvider extends IconProvider {
             ThemeManager themeManager,
             ApiWrapper apiWrapper) {
         super(context);
+        mContext = context;
         mThemeManager = themeManager;
         mApiWrapper = apiWrapper;
         setIconThemeSupported(mThemeManager.isMonoThemeEnabled());
+        registerPackageChangeListener();
     }
 
     /**
@@ -120,9 +134,36 @@ public class LauncherIconProvider extends IconProvider {
         if (mThemedIconMap != null) {
             return mThemedIconMap;
         }
+        ArrayMap<String, ThemeData> map = loadIconMapFromResource(mContext.getResources(), R.xml.grayscale_icon_map);
+        if (Utils.isPackageInstalled(mContext, LAWNICONS_PACKAGE)
+                && Utils.isPackageEnabled(mContext, LAWNICONS_PACKAGE)) {
+            Map<String, ThemeData> m = loadExternalIcons();
+            if (m != null) {
+                map.putAll(m);
+            }
+        }
+        mThemedIconMap = map;
+        return mThemedIconMap;
+    }
+
+    private Map<String, ThemeData> loadExternalIcons() {
+        try {
+            Resources res = mContext.getPackageManager().getResourcesForApplication(LAWNICONS_PACKAGE);
+            int resId = res.getIdentifier(LAWNICONS_ICON_MAP_XML, "xml", LAWNICONS_PACKAGE);
+            if (resId == 0) {
+                return null;
+            }
+            return loadIconMapFromResource(res, resId);
+        } catch (PackageManager.NameNotFoundException e) {
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private ArrayMap<String, ThemeData> loadIconMapFromResource(Resources res, int xmlResId) {
         ArrayMap<String, ThemeData> map = new ArrayMap<>();
-        Resources res = mContext.getResources();
-        try (XmlResourceParser parser = res.getXml(R.xml.grayscale_icon_map)) {
+        try (XmlResourceParser parser = res.getXml(xmlResId)) {
             final int depth = parser.getDepth();
             int type;
             while ((type = parser.next()) != XmlPullParser.START_TAG
@@ -144,7 +185,43 @@ public class LauncherIconProvider extends IconProvider {
         } catch (Exception e) {
             Log.e(TAG, "Unable to parse icon map", e);
         }
-        mThemedIconMap = map;
-        return mThemedIconMap;
+        return map;
+    }
+
+    public SafeCloseable registerPackageChangeListener() {
+        return new PackageChangeReceiver();
+    }
+
+    private class PackageChangeReceiver extends BroadcastReceiver implements SafeCloseable {
+
+        PackageChangeReceiver() {
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_PACKAGE_ADDED);
+            filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+            filter.addDataScheme("package");
+            mContext.registerReceiver(this, filter, null, MAIN_EXECUTOR.getHandler());
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            String pkg = intent.getData() != null ? intent.getData().getSchemeSpecificPart() : null;
+            if (pkg == null) return;
+            if ((Intent.ACTION_PACKAGE_ADDED.equals(action) || Intent.ACTION_PACKAGE_REMOVED.equals(action))
+                    && LAWNICONS_PACKAGE.equals(pkg)) {
+                mThemedIconMap = null;
+                if (mThemeManager.isMonoThemeEnabled()) {
+                    mThemeManager.setMonoThemeEnabled(false);
+                    mThemeManager.setMonoThemeEnabled(true);
+                }
+            }
+        }
+
+        @Override
+        public void close() {
+            try {
+                mContext.unregisterReceiver(this);
+            } catch (Exception ignored) { }
+        }
     }
 }

Mist-OS-Staging/packages_apps_Launcher3

src/com/android/launcher3/icons/LauncherIconProvider.java
@@ -15,8 +15,14 @@
  */
 package com.android.launcher3.icons;
 
+import static com.android.launcher3.util.Executors.MAIN_EXECUTOR;
+
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.graphics.drawable.AdaptiveIconDrawable;
@@ -29,6 +35,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.android.internal.util.lunaris.Utils;
 import com.android.launcher3.R;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.dagger.ApplicationContext;
@@ -36,6 +43,7 @@ import com.android.launcher3.dagger.LauncherAppSingleton;
 import com.android.launcher3.graphics.ShapeDelegate;
 import com.android.launcher3.graphics.ThemeManager;
 import com.android.launcher3.util.ApiWrapper;
+import com.android.launcher3.util.SafeCloseable;
 
 import org.xmlpull.v1.XmlPullParser;
 
@@ -57,10 +65,14 @@ public class LauncherIconProvider extends IconProvider {
     private static final String TAG = "LIconProvider";
     private static final Map<String, ThemeData> DISABLED_MAP = Collections.emptyMap();
 
+    private static final String LAWNICONS_PACKAGE = "app.lawnchair.lawnicons";
+    private static final String LAWNICONS_ICON_MAP_XML = "grayscale_icon_map";
+
     private Map<String, ThemeData> mThemedIconMap;
 
     private final ApiWrapper mApiWrapper;
     private final ThemeManager mThemeManager;
+    private final Context mContext;
 
     @Inject
     public LauncherIconProvider(
@@ -68,9 +80,11 @@ public class LauncherIconProvider extends IconProvider {
             ThemeManager themeManager,
             ApiWrapper apiWrapper) {
         super(context);
+        mContext = context;
         mThemeManager = themeManager;
         mApiWrapper = apiWrapper;
         setIconThemeSupported(mThemeManager.isMonoThemeEnabled());
+        registerPackageChangeListener();
     }
 
     /**
@@ -120,9 +134,36 @@ public class LauncherIconProvider extends IconProvider {
         if (mThemedIconMap != null) {
             return mThemedIconMap;
         }
+        ArrayMap<String, ThemeData> map = loadIconMapFromResource(mContext.getResources(), R.xml.grayscale_icon_map);
+        if (Utils.isPackageInstalled(mContext, LAWNICONS_PACKAGE)
+                && Utils.isPackageEnabled(mContext, LAWNICONS_PACKAGE)) {
+            Map<String, ThemeData> m = loadExternalIcons();
+            if (m != null) {
+                map.putAll(m);
+            }
+        }
+        mThemedIconMap = map;
+        return mThemedIconMap;
+    }
+
+    private Map<String, ThemeData> loadExternalIcons() {
+        try {
+            Resources res = mContext.getPackageManager().getResourcesForApplication(LAWNICONS_PACKAGE);
+            int resId = res.getIdentifier(LAWNICONS_ICON_MAP_XML, "xml", LAWNICONS_PACKAGE);
+            if (resId == 0) {
+                return null;
+            }
+            return loadIconMapFromResource(res, resId);
+        } catch (PackageManager.NameNotFoundException e) {
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private ArrayMap<String, ThemeData> loadIconMapFromResource(Resources res, int xmlResId) {
         ArrayMap<String, ThemeData> map = new ArrayMap<>();
-        Resources res = mContext.getResources();
-        try (XmlResourceParser parser = res.getXml(R.xml.grayscale_icon_map)) {
+        try (XmlResourceParser parser = res.getXml(xmlResId)) {
             final int depth = parser.getDepth();
             int type;
             while ((type = parser.next()) != XmlPullParser.START_TAG
@@ -144,7 +185,43 @@ public class LauncherIconProvider extends IconProvider {
         } catch (Exception e) {
             Log.e(TAG, "Unable to parse icon map", e);
         }
-        mThemedIconMap = map;
-        return mThemedIconMap;
+        return map;
+    }
+
+    public SafeCloseable registerPackageChangeListener() {
+        return new PackageChangeReceiver();
+    }
+
+    private class PackageChangeReceiver extends BroadcastReceiver implements SafeCloseable {
+
+        PackageChangeReceiver() {
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_PACKAGE_ADDED);
+            filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+            filter.addDataScheme("package");
+            mContext.registerReceiver(this, filter, null, MAIN_EXECUTOR.getHandler());
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            String pkg = intent.getData() != null ? intent.getData().getSchemeSpecificPart() : null;
+            if (pkg == null) return;
+            if ((Intent.ACTION_PACKAGE_ADDED.equals(action) || Intent.ACTION_PACKAGE_REMOVED.equals(action))
+                    && LAWNICONS_PACKAGE.equals(pkg)) {
+                mThemedIconMap = null;
+                if (mThemeManager.isMonoThemeEnabled()) {
+                    mThemeManager.setMonoThemeEnabled(false);
+                    mThemeManager.setMonoThemeEnabled(true);
+                }
+            }
+        }
+
+        @Override
+        public void close() {
+            try {
+                mContext.unregisterReceiver(this);
+            } catch (Exception ignored) { }
+        }
     }
 }